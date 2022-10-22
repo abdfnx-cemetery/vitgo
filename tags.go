3f4418dd181af580cb8e56a91e1ed27535f78f58
abdfnx <abdfnabd@gmail.com>
2022-10-22 09:20:42 +0300

    add `RenderTags` and `DevServerRedirector` function

---
+++ tags.go
@@ -0,0 +1,45 @@
+package vitgo
+
+import (
+	"bytes"
+	"html/template"
+)
+
+// RenderTags genarates the HTML tags that link a rendered
+// Go template with any Vue assets that need to be loaded.
+func (vg *VitGo) RenderTags() (template.HTML, error) {
+	var tags string
+
+	if vg.Environment == "development" {
+		if vg.Platform == "react" {
+			// react requires some extra help to load
+			tags += `
+    <script src="/src/preamble.js"></script>
+            `
+		}
+
+		tags += `
+    <script type="module" src="{{.BaseURL}}/{{ .MainModule }}"></script>
+        `
+	} else {
+		tags += `
+	<script type="module" crossorigin src="/{{ .MainModule }}"></script>
+	{{ range .Imports }}
+	<link rel="modulepreload" href="/{{.}}">
+	{{ end }}
+	{{ range .CSSModule }}
+	<link rel="stylesheet" href="/{{.}}">
+	{{ end }}
+	`
+	}
+
+	tmpl, err := template.New("tags").Parse(tags)
+	if err != nil {
+		return "", err
+	}
+
+	var buffer bytes.Buffer
+	tmpl.Execute(&buffer, vg)
+
+	return template.HTML(buffer.String()), nil
+}
